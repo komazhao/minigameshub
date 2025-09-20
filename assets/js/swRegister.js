dec5442706a693f35b4e26c03ad27f927b7250a6
Xiqiang Zhao <zhaoxq942697@gmail.com>
2025-09-21 03:27:45 +0800

    optimize chinese content

--- assets/js/swRegister.js
+++ assets/js/swRegister.js
@@ -1,6 +1,6 @@
 // Simple Service Worker registration with update prompt
 (function() {
-  const ASSET_VERSION = '20250921-05';
+  const ASSET_VERSION = '20250921-06';
 
   function createUpdateBanner(onRefresh) {
     const bar = document.createElement('div');
